8279d030fca89b8b9bfb145fdf63277f9f954afc
MARCIANO NATHAN <marcianonathan1@gmail.com>
2019-03-22 09:58:19 +0100

    initializing project

---
+++ app.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const express = require('express');
+const fs = require('fs');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}).listen(8080);
